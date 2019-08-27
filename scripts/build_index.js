2b9d74181537e1a2657212e75fe1fc773e05abe5
Sangam Kumar <ksangam68@gmail.com>
2019-08-27 23:26:19 +0530

    Add clean ui icons library

--- scripts/build_index.js
+++ scripts/build_index.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 function buildTree(startPath) {
   fs.readdir(startPath, (err, entries) => {
     if (err) throw err;
-    let moduleContent = '/** Root (Provider) component for UIIcons **/\n\n';
     entries.forEach((file) => {
       fs.readdir(`${startPath}/${file}`, (err, icons) => {
         let fileContent = `/** For ${file} icons **/\n\n`;
@@ -13,10 +12,7 @@ function buildTree(startPath) {
         });
         fs.writeFileSync(`${startPath}/${file}/index.js`, fileContent);
       });
-      moduleContent += `/** export ${file} **/\n`;
-      moduleContent += `export { default as ${file} } from './ui_icons/${file}';\n`;
     });
-    fs.writeFileSync('lib/index.js', moduleContent);
   });
 }
 
